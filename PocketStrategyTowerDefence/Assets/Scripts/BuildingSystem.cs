b8f6fa596611259a655119afeb813c367153f41c
James Mohan <c20305011@mytudublin.ie>
2022-09-24 18:23:19 +0100

    Added placing confirmation

--- PocketStrategyTowerDefence/Assets/Scripts/BuildingSystem.cs
+++ PocketStrategyTowerDefence/Assets/Scripts/BuildingSystem.cs
@@ -34,6 +34,29 @@ public class BuildingSystem : MonoBehaviour
         {
             InitializeWithObject(prefab2);
         }
+
+        if (!objectToPlace)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (CanBePlaced(objectToPlace))
+            {
+                objectToPlace.Place();
+                Vector3Int start = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+                TakeArea(start, objectToPlace.Size);
+            }
+            else
+            {
+                Destroy(objectToPlace.gameObject);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Destroy(objectToPlace.gameObject); 
+        }
     }
 
     public static Vector3 GetMouseWorldPosition()
@@ -58,6 +81,22 @@ public class BuildingSystem : MonoBehaviour
     }
 
 
+    private static TileBase[] GetTilesBlock(BoundsInt area, Tilemap tilemap)
+    {
+        TileBase[] array = new TileBase[area.size.x * area.size.y * area.size.z];
+        int counter = 0;
+
+        foreach (var v in area.allPositionsWithin)
+        {
+            Vector3Int pos = new Vector3Int(v.x, v.y, 0);
+            array[counter] = tilemap.GetTile(pos);
+            counter++;
+        }
+
+        return array;
+    }
+
+
     public void InitializeWithObject(GameObject prefab)
     {
         Vector3 position = SnapCoordinateToGrid(Vector3.zero);
@@ -67,4 +106,27 @@ public class BuildingSystem : MonoBehaviour
         obj.AddComponent<ObjectDrag>();
     }
 
+    private bool CanBePlaced(PlaceableObject placeableObject)
+    {
+        BoundsInt area = new BoundsInt();
+        area.position = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+        area.size = placeableObject.Size;
+
+        TileBase[] baseArray = GetTilesBlock(area, MainTilemap);
+
+        foreach (var b in baseArray)
+        {
+            if (b == usedTile)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void TakeArea(Vector3Int start, Vector3Int size)
+    {
+        MainTilemap.BoxFill(start, usedTile, start.x, start.y, start.x + size.x, start.y + size.y);
+    }
 }
