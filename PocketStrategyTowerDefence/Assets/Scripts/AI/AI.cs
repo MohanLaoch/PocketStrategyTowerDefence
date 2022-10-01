aa6661962501ea578d2b64ab5dc73033201ced16
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-10-01 22:11:22 +0100

    Enemies change attack priorities depending on distance
    They'll start attacking soon but dinner must go brr first

--- PocketStrategyTowerDefence/Assets/Scripts/AI/AI.cs
+++ PocketStrategyTowerDefence/Assets/Scripts/AI/AI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,64 @@ using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
-    private Transform tower;
+    public Transform target;
+    public Transform mainTower;
+    public GameObject[] turretTowers;
+
+    private float distanceToMain;
+    private float distanceToTower;
+    private float range = 5f;
+
+    private float shortestDistance;
+
+    public GameObject nearestTower = null;
+    
     public NavMeshAgent Agent;
 
     private void Awake()
     {
-        tower = GameObject.FindGameObjectWithTag("Central").GetComponent<Transform>();
+        mainTower = GameObject.FindGameObjectWithTag("Central").GetComponent<Transform>();
+        target = mainTower;
         Agent = GetComponent<NavMeshAgent>();
     }
 
     void Start()
     {
-        Agent.SetDestination(tower.position);
+        Agent.SetDestination(target.position);
+    }
+
+    private void FixedUpdate()
+    {
+        turretTowers = GameObject.FindGameObjectsWithTag("BasicTower");
+        distanceToMain = Vector3.Distance(transform.position, mainTower.position);
+        SetTarget();
+    }
+
+    private void SetTarget()
+    {
+        shortestDistance = Mathf.Infinity;
+
+        foreach (GameObject tower in turretTowers)
+        {
+            distanceToTower = Vector3.Distance(transform.position, tower.transform.position);
+
+            if (distanceToTower < distanceToMain)
+            {
+                shortestDistance = distanceToTower;
+                nearestTower = tower;
+            }
+
+            if (nearestTower != null && shortestDistance <= range)
+            {
+                target = nearestTower.transform;
+                Agent.SetDestination(target.position);
+            }
+
+            else
+            {
+                target = mainTower;
+            }
+                
+        }
     }
 }
