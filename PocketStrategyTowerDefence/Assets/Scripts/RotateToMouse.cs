153bc94c4e969bdcc75a64a9abd8d3d0fb62010f
James Mohan <c20305011@mytudublin.ie>
2022-10-01 13:18:26 +0100

    Added wizard who rotates on top of the main tower

---
+++ PocketStrategyTowerDefence/Assets/Scripts/RotateToMouse.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToMouse : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    void Update()
+    {
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+}
