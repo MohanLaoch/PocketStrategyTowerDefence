420ecbe69dfc136e36bb8f0f3723ef5166cc8836
James Mohan <c20305011@mytudublin.ie>
2022-10-03 18:03:20 +0100

    Fireball sound and cooldown

--- PocketStrategyTowerDefence/Assets/Scripts/Wizard/CreateFireBall.cs
+++ PocketStrategyTowerDefence/Assets/Scripts/Wizard/CreateFireBall.cs
@@ -7,25 +7,38 @@ public class CreateFireBall : MonoBehaviour
     public GameObject fireball;
     public Camera mainCamera;
 
+    private bool canFireball = true;
+
     public void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-            float rayLength;
-
-            
-            if (groundPlane.Raycast(cameraRay, out rayLength))
+            if (canFireball)
             {
-                Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+                Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+                Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+                float rayLength;
 
-                Instantiate(fireball, new Vector3(pointToLook.x, 1, pointToLook.z), Quaternion.identity);
-                
-                
+
+                if (groundPlane.Raycast(cameraRay, out rayLength))
+                {
+                    Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+
+                    Instantiate(fireball, new Vector3(pointToLook.x, 1, pointToLook.z), Quaternion.identity);
+
+                    FindObjectOfType<AudioManager>().Play("Fireball");
+
+                    StartCoroutine(Cooldown()); 
+                }
             }
         }
+    }
 
+    IEnumerator Cooldown()
+    {
+        canFireball = false;
+        yield return new WaitForSeconds(1);
+        canFireball = true;
     }
 
     
